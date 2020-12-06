8b1a9a55ff38fc0101f7483b683356e1bbab0fe0
marijand <marijan.divkovic@yahoo.de>
2020-12-06 14:02:12 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const errorhandler = require('errorhandler')
+
+const PORT = process.env.PORT || 4000;
+
+// Middelware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
